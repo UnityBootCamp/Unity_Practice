9ad4677959742000d0a395388096fc80e18d123a
wjdtmdgns0905 <jqhrtn33@gmail.com>
2025-05-30 10:24:51 +0900

    MoveRepository

--- 2D WarGame/Assets/02.Scripts/Unit/EnemyUnit/EnemyUnitData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-
-[CreateAssetMenu(fileName = "EnemyUnit", menuName = "Scriptable Objects/Unit/EnemyUnit")]
-public class EnemyUnitData : UnitData
-{
-    [field: SerializeField] public EnemyUnitType UnitType { get; set; }
-    [field: SerializeField] public int Weight { get; set; }
-}
